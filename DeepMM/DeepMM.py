53c1dda619cfbe640a2bb0b7aea014372fbfb79d
Dylan1021 <98151373+Dylan1021@users.noreply.github.com>
2025-05-03 14:17:42 +0800

    Update DeepMM.py

--- DeepMM/DeepMM.py
+++ DeepMM/DeepMM.py
@@ -28,23 +28,10 @@ def get_opts(options):
     correct.add_argument('--fine_tune', action='store_true', help='Use Fine-tune model')
     correct.add_argument('-t', '--threads', default=8, type=int, help='Maximum number of threads [default: 8]')
     correct.add_argument('--gpu_device', default='cuda:0', help='GPU device id')
-
-    pretrain = subparsers.add_parser('pretrain', help='Pretrain model')
-    pretrain.add_argument('--eval_dataset_path', metavar='DIR', default='', help='path to eavl dataset')
-    pretrain.add_argument('--pretrain_dataset_path', metavar='DIR', default='', help='path to dataset')
-    pretrain.add_argument('-a', '--arch', metavar='ARCH', default='resnet50', help='model architecture: resnet18 | resnet 50 (default: resnet50)')
-    pretrain.add_argument('-j', '--workers', default=32, type=int, metavar='N', help='number of data loading workers (default: 32)')
-    pretrain.add_argument('--epochs', default=100, type=int, metavar='N', help='number of total epochs to run')
-    pretrain.add_argument('-b', '--batch-size', default=64, type=int, metavar='N', help='mini-batch size (default: 64)')
-    pretrain.add_argument('--lr', '--learning-rate', default=0.0003, type=float, metavar='LR', help='initial learning rate', dest='lr')
-    pretrain.add_argument('--wd', '--weight-decay', default=1e-4, type=float, metavar='W', help='weight decay (default: 1e-4)', dest='weight_decay')
-    pretrain.add_argument('--seed', default=42, type=int, help='seed for initializing training.')
-    pretrain.add_argument('--out_dim', default=128, type=int, help='feature dimension (default: 128)')
-    pretrain.add_argument('--temperature', default=0.07, type=float, help='softmax temperature (default: 0.07)')
-    pretrain.add_argument('--n_views', default=5, type=int, metavar='N', help='Number of views for contrastive learning training.')
-    pretrain.add_argument('--gpu_device', default='cuda:0', help='GPU device id')
-    pretrain.add_argument('--checkPoint_path', type=str, default=f'./pretiran-model-weight/checkPoint_{formatted_time}', help='Checkpoint path')
-    pretrain.add_argument('--fp16-precision', action='store_true', help='Whether or not to use 16-bit precision GPU training.')
+    correct.add_argument('--file_name', default='assembly_prediction.tsv', help='Result file name')
+    correct.add_argument('--contig_file_name', default='final.contigs.fa', help='Result file name')
+    correct.add_argument('--bam_file_name', default='sort.bam', help='Result file name')
+    
     
     fine_tune = subparsers.add_parser('finetune', help='Fine-tune model')
     fine_tune.add_argument('--epochs', type=int, default = 50)
@@ -55,7 +42,7 @@ def get_opts(options):
     fine_tune.add_argument('--weight_decay', type=float, default=0.0001)
     fine_tune.add_argument('--checkPoint_path', type=str, default=f'./fine-tune-model-weight')
     fine_tune.add_argument('--pretrain_model_path', type=str, default=f'./pretrain-model-weight/checkpoint.pt')
-    fine_tune.add_argument('--fine_tune_dataset_path', type=str, default=f'')
+    fine_tune.add_argument('--finetune_dataset_path', type=str, default=f'')
 
     pretrain_data = subparsers.add_parser('pretrain_data', help='Generate pretrain dataset')
     pretrain_data.add_argument('--data_folder', type=str, default = '')
@@ -65,13 +52,13 @@ def get_opts(options):
     pretrain_data.add_argument('--alignment', type=str, default='sort.bam')
     pretrain_data.add_argument('--pretrain_dataset_path', type=str, default=f'')
 
-
     finetune_data = subparsers.add_parser('finetune_data', help='Generate finetune dataset')
     finetune_data.add_argument('--data_folder', type=str, default = '')
     finetune_data.add_argument('--feature_folder', type=str, default = '')
     finetune_data.add_argument('--label', type=str, default='all_alignments_final-contigs.tsv')
     finetune_data.add_argument('--assembly', type=str, default='final.contigs.fa')
     finetune_data.add_argument('--alignment', type=str, default='sort.bam')
+    finetune_data.add_argument('--eval_only', type=str, default=False)
 
     if not options:
         parser.print_help(sys.stderr)
