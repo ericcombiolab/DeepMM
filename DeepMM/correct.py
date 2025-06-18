dc13d5be3ea325aa85694431074b0d36b672c374
Dylan1021 <98151373+Dylan1021@users.noreply.github.com>
2025-06-18 18:26:19 +0800

    Update correct.py

--- DeepMM/correct.py
+++ DeepMM/correct.py
@@ -1,165 +1,220 @@
 import os
-import shutil
 import random
 import threading
+import subprocess
 import multiprocessing
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
+
+import torch
+import torch.nn as nn
 
 import pysam
 import numpy as np
+import pandas as pd
+from Bio import SeqIO
 from tqdm import tqdm
 
-from .tools import *
+
+from .tools import * 
+from .model import *
 from .feature_encoder import *
 
-window_len = config['window_len']
-min_contig_len = 3000
-max_contig_len = float('inf')
-skip_perc = config['skip_perc']
+np.random.seed(42)
+torch.manual_seed(42)
 random.seed(42)
+torch.cuda.manual_seed_all(42)
 
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 
-def balance_sample(folder_path):
-    ne_file = []
-    po_file = []
+def process_contig(contig_name, contig_path, align_path, model, device):
+    bp_pred = []
+    point_pred = []
+    pred = []
+    chimeric_pred = []
+    lock = threading.Lock()
 
-    for file in os.listdir(folder_path):
-        label = int(file[:-4].split('_')[-1])
-        if label == 1:
-            po_file.append(file)
-        elif label == 0:
-            ne_file.append(file)
+    with pysam.FastaFile(contig_path) as contig_file:
+        with pysam.AlignmentFile(align_path) as align_file:
+            contig_seq, read_infos = get_contig_infos(contig_name, contig_file, align_file)
+
+    contig_len = len(contig_seq)
     
-    po_num = len(po_file)
-    ne_num = len(ne_file)
+    if len(read_infos) >= 10:
+        skip_len = min(config['skip_perc'] * contig_len, 1000)
+        is_avg, filtered_readInofs = get_is_avg(read_infos, contig_seq)
+        window_len = config['window_len']
+        window_len_half = 0.5 * window_len
 
-    print(f"Positive samples: {po_num}")
-    print(f"Negative samples: {ne_num}")
+        def process_window(point):
+            try:
+                window_feature, multiple_transloc, zero_fea, window_ts, window_bp = get_feature(
+                    point=point,
+                    contig_seq=contig_seq,
+                    read_infos=filtered_readInofs,
+                    window_len=window_len,
+                    is_avg=is_avg,
+                    align_path=align_path
+                )
+                if not multiple_transloc and not zero_fea:
+                    max_bp = int(point - window_len_half) + np.argmax(window_bp) if np.max(window_bp) > 0 else point
+                    feature_maps = df_difference_nor(window_feature)
+                    input_tensor = torch.Tensor(np.array(feature_maps)).to(device).unsqueeze(0)
+                    
+                    with torch.no_grad():
+                        _, output = model(input_tensor)
+                        max_value, max_index = torch.max(output, dim=1)
+                        with lock:
+                            point_pred.append((point, output[0][1].item()))
+                            pred.append(output[0][1].item())
+                            
+                            if max_index.item() == 1 and max_value.item() >= 0.9:
+                                bp_pred.append((max_bp, output[0][1].item()))
+                                if not all(window_ts == 0):
+                                    chimeric_pred.append((max_bp, output[0][1].item()))
+                else:
+                    with lock:
+                        point_pred.append((point, 0))
+                        pred.append(0)
+            except Exception as e:
+                print(f"Error processing window at point {point}: {e}")
 
-    if po_num > ne_num:
-        majority_files = po_file
-        majority_label = 1
-        num_to_remove = po_num - ne_num
+        start_point = int(skip_len + window_len_half)
+        end_point = int(contig_len - skip_len - window_len_half)
+
+        with ThreadPoolExecutor(max_workers=config['window_threads']) as executor:
+            executor.map(process_window, range(start_point, end_point, window_len))
     else:
-        majority_files = ne_file
-        majority_label = 0
-        num_to_remove = ne_num - po_num
+        bp_pred = [(contig_len // 2, 1)]
+        pred.append(1)
+    return {
+        'contig_name': contig_name,
+        'bp_pred': bp_pred,
+        'point_pred': point_pred,
+        'max_prediction': max(pred, default=0),
+        'chimeric_pred': chimeric_pred,
+        'contig_len': contig_len,
+        'window_prediction': pred
+    }
 
-    print(f"Number of samples to remove: {num_to_remove}")
+def correct(args):
+    
+    contig_threads = args.contig_threads
+    device = args.gpu_device
+    fine_tune = args.fine_tune
+    folder_path = args.folder_path
+    model_weight_path = args.model_weight_path
+    file_name = args.file_name
+    contig_file_name = args.contig_file_name
+    bam_file_name = args.bam_file_name
+    correct_all = args.correct_all
+    score_cut = args.threshold
+    config['window_threads'] = args.window_threads
+    
 
-    random_selection = random.sample(majority_files, k=num_to_remove)
+    multiprocessing.set_start_method('spawn')
+    contig_path = os.path.join(folder_path, contig_file_name)
+    align_path = os.path.join(folder_path, bam_file_name)
 
-    for file in tqdm(random_selection, desc="Removing excess samples"):
-        os.remove(os.path.join(folder_path, file))
+    contig_fai_path = f'{contig_path}.fai'
+    align_fai_path = f'{align_path}.bai'
 
-    po_file = [file for file in os.listdir(folder_path) if int(file[:-4].split('_')[-1]) == 1]
-    ne_file = [file for file in os.listdir(folder_path) if int(file[:-4].split('_')[-1]) == 0]
-    print(f"Final Positive samples: {len(po_file)}")
-    print(f"Final Negative samples: {len(ne_file)}")
+    touch_cmd = [
+        'touch', contig_fai_path
+    ]
+    subprocess.run(touch_cmd)
+    touch_cmd = [
+        'touch', align_fai_path
+    ]
+    subprocess.run(touch_cmd)
 
+    output_folder_path = f'{folder_path}/deepmm_output'
+    os.makedirs(output_folder_path, exist_ok=True)
+    
+    model = LinearEvaluation(ResNetSimCLR(base_model='resnet50', out_dim=128))
 
-def save_feature(window_name, window_feature, label, file_folder):
-    file = f'{window_name}_{label}.npy'
-    if file not in os.listdir(file_folder):
-        np.save(os.path.join(file_folder, file), window_feature)
-
-def get_positive_window(proper_bp_contig_names, contig_path, align_path, positive_contig, comm, file_folder):
-    with pysam.FastaFile(contig_path) as contig_file:
-        with pysam.AlignmentFile(align_path, 'rb') as align_file:
-            for contig_name in tqdm(proper_bp_contig_names, desc = f'Comm: {comm}, positive_cases'):
-                contig_seq, read_infos = get_contig_infos(contig_name, contig_file, align_file)
-                if len(read_infos) >= 10:
-                    contig_len = len(contig_seq)
-                    skip_len = skip_perc * contig_len if skip_perc * contig_len < 1000 else 1000
-                    is_avg = get_is_avg(read_infos, contig_seq)
-                    for label_point in positive_contig[contig_name]['break_point']:
-                        #* pick proper break point label
-                        if skip_len + window_len / 2 <= label_point < contig_len - skip_len - window_len / 2:
-                            #* Forward sliding
-                            random_number = random.random()
-                            point = int(label_point - (window_len / 2) * random_number)
-                            window_feature, multiple_transloc, zero_fea, window_ts, window_bp = get_feature(point = point, contig_seq=contig_seq, read_infos = read_infos, window_len = config['window_len'], is_avg=is_avg, align_path = align_path)
-                            if not multiple_transloc and not zero_fea:
-                                window_name = f"comm_{comm}_{contig_name}_{point}"
-                                label = 1
-                                save_feature(window_name, window_feature, label, file_folder)
-
-                            #* Midle one
-                            point = int(label_point)
-                            window_feature, multiple_transloc, zero_fea, window_ts, window_bp = get_feature(point = point, contig_seq=contig_seq, read_infos = read_infos, window_len = config['window_len'], is_avg=is_avg, align_path = align_path)
-                            if not multiple_transloc and not zero_fea:
-                                window_name = f"comm_{comm}_{contig_name}_{point}"
-                                label = 1
-                                save_feature(window_name, window_feature, label, file_folder)
-
-                            #* Backward sliding
-                            point = int(label_point + (window_len / 2) * random_number)
-                            window_feature, multiple_transloc, zero_fea, window_ts, window_bp = get_feature(point = point, contig_seq=contig_seq, read_infos = read_infos, window_len = config['window_len'], is_avg=is_avg, align_path = align_path)
-                            if not multiple_transloc and not zero_fea:
-                                window_name = f"comm_{comm}_{contig_name}_{point}"
-                                label = 1
-                                save_feature(window_name, window_feature, label, file_folder)
-
-def get_negative_window(negative_contig_names, contig_path, align_path, comm, file_folder):
-    with pysam.FastaFile(contig_path) as contig_file:
-        with pysam.AlignmentFile(align_path, 'rb') as align_file:
-            for contig_name in tqdm(negative_contig_names, desc = f'Comm: {comm}, negative_cases'):
-                contig_seq, read_infos = get_contig_infos(contig_name, contig_file, align_file)
-                contig_len = len(contig_seq)
-                if len(read_infos) >= 10:
-                    skip_len = skip_perc * contig_len if skip_perc * contig_len < 1000 else 1000
-                    is_avg = get_is_avg(read_infos, contig_seq)
-                    #* Forward 
-                    for point in range(int(skip_len + window_len / 2), int(contig_len - skip_len - window_len / 2), window_len):
-                        window_feature, multiple_transloc, zero_fea, window_ts, window_bp = get_feature(point = point, contig_seq=contig_seq, read_infos = read_infos, window_len = config['window_len'], is_avg=is_avg, align_path = align_path)
-                        if not multiple_transloc and not zero_fea:
-                            window_name = f"comm_{comm}_{contig_name}_{point}"
-                            label = 0
-                            save_feature(window_name, window_feature, label, file_folder)
-
-
-def get_training_data(args, status):
-    data_folder = args.data_folder
-    feature_folder = args.feature_folder
-    label = args.label
-    assembly = args.assembly
-    alignment = args.alignment
-    file_folder = f'{feature_folder}/{status}'
-    os.makedirs(file_folder, exist_ok=True)
-    community = os.listdir(data_folder)
-    community.sort(key=int)
-    pool = multiprocessing.Pool(processes=30)
-    for comm in community:
-        label_path = os.path.join(data_folder, comm, label)
-        contig_path = os.path.join(data_folder, comm, assembly)
-        align_path = os.path.join(data_folder, comm, alignment)
-        negative_contig, positive_contig = extract_contig_info(label_path)
-
-        negative_contig_names = [contig_name for contig_name in negative_contig.keys() if min_contig_len <= negative_contig[contig_name]['contig_len'] < max_contig_len]
-        positive_contig_names = [contig_name for contig_name in positive_contig.keys() if  min_contig_len <= positive_contig[contig_name]['contig_len'] < max_contig_len]
-
-        po_train_end = int(0.8 * len(positive_contig_names))
-        ne_train_end = int(0.8 * len(negative_contig_names))
-
-        if status == 'train':
-            positive_contig_names = positive_contig_names[:po_train_end]
-            negative_contig_names = negative_contig_names[:ne_train_end]
-        elif status == 'eval':
-            positive_contig_names = positive_contig_names[po_train_end:]
-            negative_contig_names = negative_contig_names[ne_train_end:]
-
-        max_connections = threading.Semaphore(2)
-        po_result = pool.apply_async(get_positive_window, (positive_contig_names, contig_path, align_path, positive_contig, comm, file_folder))
-        ne_result = pool.apply_async(get_negative_window, (negative_contig_names, contig_path, align_path, comm, file_folder))
-
-    pool.close()
-    pool.join()
-    print('Data cleaning ... ')
-    balance_sample(folder_path = f'{feature_folder}/{status}')
-
-def get_fine_tune_dataset(args):
-    if not args.eval_only:
-        get_training_data(args, 'train')
-        get_training_data(args, 'eval')
+    if fine_tune:
+        print("Using Fine-tune model")
+        model = FinetuneModel(model)
     else:
-        get_training_data(args, 'eval')
+        print("Using Default model")
 
+    model.load_state_dict(torch.load(model_weight_path, map_location = device, weights_only=True))
+    model = model.to(device)
+    model.eval()   
+
+    contig_test = {}
+    total_contig_name = []
+    with pysam.FastaFile(contig_path) as contig_file:
+        all_contig_names = contig_file.references
+        for contig_name in all_contig_names:
+            contig_seq = contig_file.fetch(contig_name)
+            if config['min_contig_length'] <= len(contig_seq) < config['max_contig_length']:
+                total_contig_name.append(contig_name)
+
+    process_contig_partial = partial(process_contig, contig_path=contig_path, align_path=align_path, model = model, device = device)
+
+    with multiprocessing.Pool(processes=contig_threads) as pool:
+        results = list(tqdm(pool.imap(process_contig_partial, total_contig_name), total=len(total_contig_name), desc='Predicting ...'))
+
+    contig_test = {result['contig_name']: {'bp_pred': result['bp_pred'], 'point_pred': result['point_pred'],
+                                            'max_prediction': result['max_prediction'], 'contig_len': result['contig_len'],
+                                            'chimeric_pred': result['chimeric_pred'], 'window_prediction': result['window_prediction']
+                                            } for result in results}
+
+    breakcontigs = []
+    for contig_name in contig_test.keys():
+        if len(contig_test[contig_name]['bp_pred']) > 0:
+            breakcontigs.append(contig_name)
+            
+    with open(f'{output_folder_path}/{file_name}', 'w') as f:
+        f.write('Assembly\tBreakPoint\tPrediction\tLength\tChimeric_BreakPoint\tChimeric_Prediction\n')
+        for contig_name in total_contig_name:
+            if contig_name in breakcontigs:
+                sorted_data = sorted(contig_test[contig_name]['bp_pred'], key=lambda x: x[1])
+                bp = sorted_data[-1][0]
+                bp_prediction = sorted_data[-1][1]
+                if len(contig_test[contig_name]['chimeric_pred']) > 0:
+                    sorted_data = sorted(contig_test[contig_name]['chimeric_pred'], key=lambda x: x[1])
+                    chi_point = sorted_data[-1][0]
+                    chi_prediction = sorted_data[-1][1]
+                    f.write(contig_name + '\t' + str(bp) + '\t' + str(bp_prediction) + '\t' + str(contig_test[contig_name]['contig_len']) + '\t' + str(chi_point) + '\t' + str(chi_prediction) + '\n')
+                else:
+                    f.write(contig_name + '\t' + str(bp) + '\t' + str(bp_prediction) + '\t' + str(contig_test[contig_name]['contig_len']) + '\t' + '-1' + '\t' + '0' + '\n')
+            else:
+                f.write(contig_name + '\t' + '-1' + '\t' + str(contig_test[contig_name]['max_prediction']) + '\t' + str(contig_test[contig_name]['contig_len']) + '\t' + '-1' + '\t' + '0' + '\n')
+
+    corrected_contig_file = os.path.join(output_folder_path, "corrected_contigs.fa")
+    original_file = f'{folder_path}/{contig_file_name}'
+    input_file = SeqIO.parse(original_file, "fasta")
+    df = pd.read_csv(f'{output_folder_path}/{file_name}', sep='\t')
+    
+    if not correct_all:
+        print('Correcting chimeric misassembly')
+        breakcontigs = df.loc[df['Chimeric_Prediction'] > score_cut,]
+        breakcontigs = breakcontigs.loc[breakcontigs['Chimeric_BreakPoint'] > config['min_split_length'], ]
+        breakcontigs = breakcontigs.loc[(breakcontigs['Length'] - breakcontigs['Chimeric_BreakPoint']) > config['min_split_length'], ]
+        breakcontigs = list(np.unique(breakcontigs['Assembly']))
+    else:
+        print('Correcting all misassemblies')
+        breakcontigs = df.loc[df['Prediction'] > score_cut,]
+        breakcontigs = breakcontigs.loc[breakcontigs['BreakPoint'] > config['min_split_length'], ]
+        breakcontigs = breakcontigs.loc[(breakcontigs['Length'] - breakcontigs['BreakPoint']) > config['min_split_length'], ]
+        breakcontigs = list(np.unique(breakcontigs['Assembly']))
+    
+    with open(corrected_contig_file, "w") as corrected_file:
+        for record in tqdm(input_file, desc = f'Correting ... '):
+            if record.id in breakcontigs:
+                if not correct_all:
+                    bp = int(df.loc[df['Assembly'] == record.id, 'Chimeric_BreakPoint'].values[0])
+                else:
+                    bp = int(df.loc[df['Assembly'] == record.id, 'BreakPoint'].values[0])
+                corrected_file.write(">" + record.id + "_1\n")
+                corrected_file.write(str(record.seq[:bp]) + "\n")
+                corrected_file.write(">" + record.id + "_2\n")
+                corrected_file.write(str(record.seq[bp:]) + "\n")
+            else:
+                corrected_file.write(">" + record.id + "\n")
+                corrected_file.write(str(record.seq) + "\n")
+    print(f"A total of " + str(len(breakcontigs)) + " misassembled contigs are corrected")
